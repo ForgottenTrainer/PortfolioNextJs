45cbf7bc1eef594bbfbe4e0f0f84e2f7185eb987
forgotten <rogeliogonzalezperez@MacBook-Air-de-forgotten.local>
2023-09-28 14:15:12 -0500

    resp

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
       <footer className="">
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" class="hover:underline">RogerDev™</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="/" className="hover:underline">RogerDev™</a>. All Rights Reserved.
         </span>
         </div>
       </footer>
