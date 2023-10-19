64049aff03c3ad22e8f94c19ed5de0e07dd3f176
forgotten <rogeliogonzalezperez@Airdeforgotten.lan>
2023-10-19 12:55:52 -0500

    new image

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
           </h1>
           <p className="text-[#ADB7BE] text-base  mb-6 lg:text-xl">Fullstack web developer passionate about the world of technology and development</p>
           <div className="text-center sm:text-left">
-            <a href='https://www.linkedin.com/in/rogelio-gonz%C3%A1lez-p%C3%A9rez-891493212/' className="w-full text-white sm:w-fit mb-3 px-6 py-3 rounded-full mr-4 bg-gradient-to-tl from-purple-900 via-indigo-700 via-blue-900 to-blue-400 hover:bg-slate-200 text-black">
+            <a href='https://www.linkedin.com/in/rogelio-gonz%C3%A1lez-perez-jr-891493212/' className="w-full text-white sm:w-fit mb-3 px-6 py-3 rounded-full mr-4 bg-gradient-to-tl from-purple-900 via-indigo-700 via-blue-900 to-blue-400 hover:bg-slate-200 text-black">
               Hire me
             </a>
             <a href='cv.pdf' className="w-full sm:w-fit px-6 py-3 rounded-full bg-transparent hover:bg-slate-800 text-white border border-white">
@@ -38,11 +38,11 @@ const HeroSection = () => {
         <div className="col-span-12 sm:col-span-4 place-self-center mt-4 lg:mt-0">
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/Roger.png"
+              src="/images/Ro.png"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
               alt="hero image"
-              width={300}
-              height={300}
+              width={350}
+              height={350}
             />
           </div>
         </div>
